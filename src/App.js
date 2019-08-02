42b2010e58a0af7ed591ccb987556fb17859d018
1262862038 <1262862038@qq.com>
2019-08-02 20:49:40 +0800

    项目环境搭建

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import Home from './pages/Home'
+import City from './pages/City'
+import Map from './pages/Map'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="app">我是app</div>
+        <ul>
+          <li>
+            <Link to="/home">首页</Link>
+          </li>
+          <li>
+            <Link to="/city">城市</Link>
+          </li>
+          <li>
+            <Link to="/map">地图</Link>
+          </li>
+        </ul>
+        <Route path="/home" component={Home} />
+        <Route path="/city" component={City} />
+        <Route path="/map" component={Map} />
+      </Router>
+    )
+  }
 }
-
-export default App;
+export default App
---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'antd-mobile'
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        Home组件
+        <Button>按钮</Button>
+      </div>
+    )
+  }
+}
+export default Home
